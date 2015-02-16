ae3c7b21236028fe0903bd2493e843de99586bf3
Lyatus (ENJMIN) <lutopia@hotmail.fr>
2015-02-16 17:35:43 +0100

    Cube, sphere, sun

--- _minecraft/src/main.cpp
+++ _minecraft/src/main.cpp
@@ -38,8 +38,7 @@ GUISlider * g_slider;
 //////////////////////////////////////////////////////////////////////////
 // GESTION APPLICATION
 //////////////////////////////////////////////////////////////////////////
-void update(void)
-{
+void update(void) {
 	float elapsed = g_timer->getElapsedSeconds(true);
 
 	static float g_eval_elapsed = 0;
@@ -47,8 +46,7 @@ void update(void)
 	//Calcul des fps
 	g_elapsed_fps += elapsed;
 	g_nb_frames++;
-	if(g_elapsed_fps > 1.0)
-	{
+	if (g_elapsed_fps > 1.0) {
 		LabelFps->Text = std::string("FPS : ") + toString(g_nb_frames);
 		g_elapsed_fps -= 1.0f;
 		g_nb_frames = 0;
@@ -59,218 +57,326 @@ void update(void)
 }
 
 
-void render2d(void)
-{
+void render2d(void) {
 	g_screen_manager->render();
 }
 
-void renderObjects(void)
-{
+void renderObjects(void) {
 	//Rendu des axes
 	glDisable(GL_LIGHTING);
 
+	//Dessin des axes : on dessine des lignes
 	glBegin(GL_LINES);
-	glColor3d(1,0,0);
-	glVertex3d(0,0,0);
-	glVertex3d(10000,0,0);
-	glColor3d(0,1,0);
-	glVertex3d(0,0,0);
-	glVertex3d(0,10000,0);
-	glColor3d(0,0,1);
-	glVertex3d(0,0,0);
-	glVertex3d(0,0,10000);
+
+	//Première primitive : axe des x
+	glColor3d(1, 0, 0);
+	glVertex3d(0, 0, 0);
+	glVertex3d(10000, 0, 0);
+
+	//Deuxième primitive : axe des y
+	glColor3d(0, 1, 0);
+	glVertex3d(0, 0, 0);
+	glVertex3d(0, 10000, 0);
+
+	//Troisième primitive : axe des z
+	glColor3d(0, 0, 1);
+	glVertex3d(0, 0, 0);
+	glVertex3d(0, 0, 10000);
 	glEnd();
-	
+
+	glEnable(GL_LIGHTING);
+
+	//Speculaire
+	GLfloat whiteSpecularMaterial[] = { 0.3, 0.3, 0.3, 1.0 };
+	glMaterialfv(GL_FRONT, GL_SPECULAR, whiteSpecularMaterial);
+	GLfloat mShininess = 100;
+	glMaterialf(GL_FRONT, GL_SHININESS, mShininess);
+
+	//Emissive
+	GLfloat black[] = { 0.0, 0.0, 0.0, 1.0 };
+
+	//Ambient
+	GLfloat materialAmbient[] = { 0.3, 0.3, 0.3, 1.0 };
+	glMaterialfv(GL_FRONT, GL_AMBIENT, materialAmbient);
+
+	NYVert3Df lightPos(0, 0, 10), sunNorm(0, 1, 0);
+	lightPos.rotate(sunNorm, NYRenderer::_DeltaTimeCumul / 10);
+	float position[4] = { lightPos.X, lightPos.Y, lightPos.Z, 1 }; // w = 1 donc c'est une point light (w=0 -> directionelle, point à l'infini)
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	float diffuse[4] = { 0.5f, 0.5f, 0.5f };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+
+	glMaterialfv(GL_FRONT, GL_EMISSION, black);
+
+	//A rajouter pour debug : rotation dans le temps
+	glPushMatrix();
+	glRotatef(NYRenderer::_DeltaTimeCumul * 100, g_slider->Value*10.0f, 1, cos(NYRenderer::_DeltaTimeCumul));
+	glRotatef(45, 0, 0, 1);
+	glTranslated(2, 0, 0);
+
+	//Pour dessiner des quads
+	glBegin(GL_QUADS);
+
+	//Back face
+	GLfloat backDiffuse[] = { 0, 1.0, 1.0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, backDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, backDiffuse);
+	glNormal3f(-1, 0, 0);
+	glVertex3d(-1, -1, -1);
+	glVertex3d(-1, -1, 1);
+	glVertex3d(-1, 1, 1);
+	glVertex3d(-1, 1, -1);
+
+
+	//Front face
+	GLfloat frontDiffuse[] = { 1.0, 0, 0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, frontDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, frontDiffuse);
+	glNormal3f(1, 0, 0);
+	glVertex3d(1, -1, -1);
+	glVertex3d(1, 1, -1);
+	glVertex3d(1, 1, 1);
+	glVertex3d(1, -1, 1);
+
+	//Bottom face
+	GLfloat bottomDiffuse[] = { 1.0, 1.0, 0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, bottomDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, bottomDiffuse);
+	glNormal3f(0, 0, -1);
+	glVertex3d(-1, -1, -1);
+	glVertex3d(-1, +1, -1);
+	glVertex3d(+1, +1, -1);
+	glVertex3d(+1, -1, -1);
+
+	//Top face
+	GLfloat topDiffuse[] = { 0, 0, 1.0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, topDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, topDiffuse);
+	glNormal3f(0, 0, 1);
+	glVertex3d(-1, -1, +1);
+	glVertex3d(+1, -1, +1);
+	glVertex3d(+1, +1, +1);
+	glVertex3d(-1, +1, +1);
+
+	//Left face
+	GLfloat leftDiffuse[] = { 1.0, 0, 1.0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, leftDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, leftDiffuse);
+	glNormal3f(0, -1, 0);
+	glVertex3d(-1, -1, -1);
+	glVertex3d(+1, -1, -1);
+	glVertex3d(+1, -1, +1);
+	glVertex3d(-1, -1, +1);
+
+	//Right face
+	GLfloat rightDiffuse[] = { 0, 1.0, 0, 1.0 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, rightDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, rightDiffuse);
+	glNormal3f(0, 1, 0);
+	glVertex3d(-1, +1, -1);
+	glVertex3d(-1, +1, +1);
+	glVertex3d(+1, +1, +1);
+	glVertex3d(+1, +1, -1);
+
+	glEnd();
+
+	GLfloat sphereDiffuse[] = { 1, 1, 1, 0.5 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, sphereDiffuse);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, sphereDiffuse);
+	glutSolidSphere(2, 16, 16);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(position[0], position[1], position[2]);
+	GLfloat orange[] = { 1.0, 0.8, 0, 0.8 };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, orange);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, orange);
+	glMaterialfv(GL_FRONT, GL_EMISSION, orange);
+	glutSolidSphere(.5, 8, 8);
+	glPopMatrix();
 }
 
-void setLights(void)
-{
+void setLights(void) {
 	//On active la light 0
+	glEnable(GL_BLEND_SRC);
 	glEnable(GL_LIGHT0);
 
-	//On définit une lumière directionelle (un soleil)
-	float direction[4] = {0,0,1,0}; ///w = 0 donc elle est a l'infini
-	glLightfv(GL_LIGHT0, GL_POSITION, direction );
-	float color[4] = {0.5f,0.5f,0.5f};
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, color );
-	float color2[4] = {0.3f,0.3f,0.3f};
-	glLightfv(GL_LIGHT0, GL_AMBIENT, color2 );
-	float color3[4] = {0.3f,0.3f,0.3f};
-	glLightfv(GL_LIGHT0, GL_SPECULAR, color3 );
+	//On définit une lumière
+	float specular[4] = { 0.5f, 0.5f, 0.5f };
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+
+	float ambient[4] = { 0.1f, 0.1f, 0.1f };
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+
+	NYColor skyColor(0, 181.f / 255.f, 221.f / 255.f, 1);
+	g_renderer->setBackgroundColor(skyColor);
 }
 
-void resizeFunction(int width, int height)
-{
+void resizeFunction(int width, int height) {
 	glViewport(0, 0, width, height);
-	g_renderer->resize(width,height);
+	g_renderer->resize(width, height);
 }
 
 //////////////////////////////////////////////////////////////////////////
 // GESTION CLAVIER SOURIS
 //////////////////////////////////////////////////////////////////////////
 
-void specialDownFunction(int key, int p1, int p2)
-{
+void specialDownFunction(int key, int p1, int p2) {
 	//On change de mode de camera
-	if(key == GLUT_KEY_LEFT)
-	{
+	if (key == GLUT_KEY_LEFT) {
 	}
 
 }
 
-void specialUpFunction(int key, int p1, int p2)
-{
+void specialUpFunction(int key, int p1, int p2) {
 
 }
 
-void keyboardDownFunction(unsigned char key, int p1, int p2)
-{
-	if(key == VK_ESCAPE)
-	{
-		glutDestroyWindow(g_main_window_id);	
+void keyboardDownFunction(unsigned char key, int p1, int p2) {
+	if (key == VK_ESCAPE) {
+		glutDestroyWindow(g_main_window_id);
 		exit(0);
 	}
 
-	if(key == 'f')
-	{
-		if(!g_fullscreen){
+	if (key == 'f') {
+		if (!g_fullscreen) {
 			glutFullScreen();
 			g_fullscreen = true;
-		} else if(g_fullscreen){
+		}
+		else if (g_fullscreen) {
 			glutLeaveGameMode();
 			glutLeaveFullScreen();
 			glutReshapeWindow(g_renderer->_ScreenWidth, g_renderer->_ScreenWidth);
-			glutPositionWindow(0,0);
+			glutPositionWindow(0, 0);
 			g_fullscreen = false;
 		}
-	}	
+	}
+	else if (key == 'z') {
+		g_renderer->_Camera->moveForward(.1f);
+	}
 }
 
-void keyboardUpFunction(unsigned char key, int p1, int p2)
-{
+void keyboardUpFunction(unsigned char key, int p1, int p2) {
 }
 
-void mouseWheelFunction(int wheel, int dir, int x, int y)
-{
-	
+void mouseWheelFunction(int wheel, int dir, int x, int y) {
+	g_renderer->_Camera->move(NYVert3Df(0, 0, (wheel*dir) / 2.f));
 }
 
-void mouseFunction(int button, int state, int x, int y)
-{
+void mouseFunction(int button, int state, int x, int y) {
 	//Gestion de la roulette de la souris
-	if((button & 0x07) == 3 && state)
-		mouseWheelFunction(button,1,x,y);
-	if((button & 0x07) == 4 && state)
-		mouseWheelFunction(button,-1,x,y);
+	if ((button & 0x07) == 3 && state)
+		mouseWheelFunction(button, 1, x, y);
+	if ((button & 0x07) == 4 && state)
+		mouseWheelFunction(button, -1, x, y);
 
 	//GUI
 	g_mouse_btn_gui_state = 0;
-	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 		g_mouse_btn_gui_state |= GUI_MLBUTTON;
-	
+
 	bool mouseTraite = false;
-	mouseTraite = g_screen_manager->mouseCallback(x,y,g_mouse_btn_gui_state,0,0);
+	mouseTraite = g_screen_manager->mouseCallback(x, y, g_mouse_btn_gui_state, 0, 0);
 }
 
-void mouseMoveFunction(int x, int y, bool pressed)
-{
+int oldx = 0, oldy = 0;
+void mouseMoveFunction(int x, int y, bool pressed) {
 	bool mouseTraite = false;
 
-	mouseTraite = g_screen_manager->mouseCallback(x,y,g_mouse_btn_gui_state,0,0);
-	if(pressed && mouseTraite)
-	{
+	mouseTraite = g_screen_manager->mouseCallback(x, y, g_mouse_btn_gui_state, 0, 0);
+	if (pressed && mouseTraite) {
 		//Mise a jour des variables liées aux sliders
 	}
 
+	if (oldx || oldy) {
+		if (pressed) {
+			g_renderer->_Camera->move(NYVert3Df((x - oldx) / 8.f, (y - oldy) / 8.f, 0));
+		}
+		else {
+			g_renderer->_Camera->rotateAround(-(x - oldx) / 180.f);
+			g_renderer->_Camera->rotateUpAround(-(y - oldy) / 180.f);
+		}
+	}
+	oldx = x;
+	oldy = y;
 }
 
-void mouseMoveActiveFunction(int x, int y)
-{
-	mouseMoveFunction(x,y,true);
+void mouseMoveActiveFunction(int x, int y) {
+	mouseMoveFunction(x, y, true);
 }
-void mouseMovePassiveFunction(int x, int y)
-{
-	mouseMoveFunction(x,y,false);
+void mouseMovePassiveFunction(int x, int y) {
+	mouseMoveFunction(x, y, false);
 }
 
 
-void clickBtnParams (GUIBouton * bouton)
-{
+void clickBtnParams(GUIBouton * bouton) {
 	g_screen_manager->setActiveScreen(g_screen_params);
 }
 
-void clickBtnCloseParam (GUIBouton * bouton)
-{
+void clickBtnCloseParam(GUIBouton * bouton) {
 	g_screen_manager->setActiveScreen(g_screen_jeu);
 }
 
 /**
   * POINT D'ENTREE PRINCIPAL
   **/
-int main(int argc, char* argv[])
-{ 
+int main(int argc, char* argv[]) {
 	LogConsole::createInstance();
 
 	int screen_width = 800;
 	int screen_height = 600;
 
-	glutInit(&argc, argv); 
-	glutInitContextVersion(3,0);
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 0);
 	glutSetOption(
 		GLUT_ACTION_ON_WINDOW_CLOSE,
 		GLUT_ACTION_GLUTMAINLOOP_RETURNS
 		);
 
-	glutInitWindowSize(screen_width,screen_height);
-	glutInitWindowPosition (0, 0);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE );
+	glutInitWindowSize(screen_width, screen_height);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
 
 	glEnable(GL_MULTISAMPLE);
 
-	Log::log(Log::ENGINE_INFO, (toString(argc) + " arguments en ligne de commande.").c_str());	
+	Log::log(Log::ENGINE_INFO, (toString(argc) + " arguments en ligne de commande.").c_str());
 	bool gameMode = true;
-	for(int i=0;i<argc;i++)
-	{
-		if(argv[i][0] == 'w')
-		{
-			Log::log(Log::ENGINE_INFO,"Arg w mode fenetre.\n");
+	for (int i = 0; i < argc; i++) {
+		if (argv[i][0] == 'w') {
+			Log::log(Log::ENGINE_INFO, "Arg w mode fenetre.\n");
 			gameMode = false;
 		}
 	}
 
-	if(gameMode)
-	{
+	if (gameMode) {
 		int width = glutGet(GLUT_SCREEN_WIDTH);
 		int height = glutGet(GLUT_SCREEN_HEIGHT);
-		
+
 		char gameModeStr[200];
-		sprintf(gameModeStr,"%dx%d:32@60",width,height);
+		sprintf(gameModeStr, "%dx%d:32@60", width, height);
 		glutGameModeString(gameModeStr);
 		g_main_window_id = glutEnterGameMode();
 	}
-	else
-	{
+	else {
 		g_main_window_id = glutCreateWindow("MyNecraft");
-		glutReshapeWindow(screen_width,screen_height);
+		glutReshapeWindow(screen_width, screen_height);
 	}
 
-	if(g_main_window_id < 1) 
-	{
-		Log::log(Log::ENGINE_ERROR,"Erreur creation de la fenetre.");
+	if (g_main_window_id < 1) {
+		Log::log(Log::ENGINE_ERROR, "Erreur creation de la fenetre.");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	GLenum glewInitResult = glewInit();
 
-	if (glewInitResult != GLEW_OK)
-	{
-		Log::log(Log::ENGINE_ERROR,("Erreur init glew " + std::string((char*)glewGetErrorString(glewInitResult))).c_str());
-		_cprintf("ERROR : %s",glewGetErrorString(glewInitResult));
+	if (glewInitResult != GLEW_OK) {
+		Log::log(Log::ENGINE_ERROR, ("Erreur init glew " + std::string((char*)glewGetErrorString(glewInitResult))).c_str());
+		_cprintf("ERROR : %s", glewGetErrorString(glewInitResult));
 		exit(EXIT_FAILURE);
 	}
 
 	//Affichage des capacités du système
-	Log::log(Log::ENGINE_INFO,("OpenGL Version : " + std::string((char*)glGetString(GL_VERSION))).c_str());
+	Log::log(Log::ENGINE_INFO, ("OpenGL Version : " + std::string((char*)glGetString(GL_VERSION))).c_str());
 
 	glutDisplayFunc(update);
 	glutReshapeFunc(resizeFunction);
@@ -293,15 +399,15 @@ int main(int argc, char* argv[])
 
 	//On applique la config du renderer
 	glViewport(0, 0, g_renderer->_ScreenWidth, g_renderer->_ScreenHeight);
-	g_renderer->resize(g_renderer->_ScreenWidth,g_renderer->_ScreenHeight);
-	
+	g_renderer->resize(g_renderer->_ScreenWidth, g_renderer->_ScreenHeight);
+
 	//Ecran de jeu
 	uint16 x = 10;
 	uint16 y = 10;
-	g_screen_jeu = new GUIScreen(); 
+	g_screen_jeu = new GUIScreen();
 
 	g_screen_manager = new GUIScreenManager();
-		
+
 	//Bouton pour afficher les params
 	BtnParams = new GUIBouton();
 	BtnParams->Titre = std::string("Params");
@@ -330,8 +436,8 @@ int main(int argc, char* argv[])
 	g_screen_params->addElement(btnClose);
 
 	y += btnClose->Height + 1;
-	y+=10;
-	x+=10;
+	y += 10;
+	x += 10;
 
 	GUILabel * label = new GUILabel();
 	label->X = x;
@@ -342,21 +448,21 @@ int main(int argc, char* argv[])
 	y += label->Height + 1;
 
 	g_slider = new GUISlider();
-	g_slider->setPos(x,y);
-	g_slider->setMaxMin(1,0);
+	g_slider->setPos(x, y);
+	g_slider->setMaxMin(1, 0);
 	g_slider->Visible = true;
 	g_screen_params->addElement(g_slider);
 
 	y += g_slider->Height + 1;
-	y+=10;
+	y += 10;
 
 	//Ecran a rendre
 	g_screen_manager->setActiveScreen(g_screen_jeu);
-	
+
 	//Init Camera
-	g_renderer->_Camera->setPosition(NYVert3Df(10,10,10));
-	g_renderer->_Camera->setLookAt(NYVert3Df(0,0,0));
-	
+	g_renderer->_Camera->setPosition(NYVert3Df(10, 10, 10));
+	g_renderer->_Camera->setLookAt(NYVert3Df(0, 0, 0));
+
 
 	//Fin init moteur
 
@@ -366,11 +472,11 @@ int main(int argc, char* argv[])
 
 	//Init Timer
 	g_timer = new NYTimer();
-	
+
 	//On start
 	g_timer->start();
 
-	glutMainLoop(); 
+	glutMainLoop();
 
 	return 0;
 }
